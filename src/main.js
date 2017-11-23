92e33ee58245882adf4feb4baf2ea54bf39569dd
Ashwini Kadam <a.kadam@cablelabs.com>
2017-11-23 14:38:07 -0800

    cleanup

--- src/main.js
+++ src/main.js
@@ -9,15 +9,7 @@ import router from './router'
 import Vuelidate from 'vuelidate'
 
 Vue.use(Vuelidate)
-Vue.use(Vuetify, { theme: {
-  primary: '#ee44aa',
-  secondary: '#424242',
-  accent: '#82B1FF',
-  error: '#FF5252',
-  info: '#2196F3',
-  success: '#4CAF50',
-  warning: '#FFC107'
-}})
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
