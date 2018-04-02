b6ed115fb80136e5eb4d3f1b16dba0a14417f05e
Ashwini Kadam <a.kadam@cablelabs.com>
2018-04-02 15:58:52 -0700

    Store updates

--- client/src/store/main.js
+++ client/src/store/main.js
@@ -1,13 +1,21 @@
 import axios from 'axios'
-import { findIndex, propEq, find, lensPath, view, set, omit } from 'ramda'
+import { findIndex, propEq, find, lensPath, view, set, omit, merge } from 'ramda'
+
 const setState = prop => (state, value) => { state[prop] = value }
 const micronetsUrl = `${process.env.BASE_URL}/micronets`
-const apiInit = { crossDomain: true, headers: { 'Content-type': 'application/json' } }
+const apiInit = {crossDomain: true, headers: {'Content-type': 'application/json'}}
+const msoPortalApiInit = {
+  crossDomain: true,
+  headers: {
+    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6ImFjY2VzcyIsInR5cGUiOiJhY2Nlc3MifQ.eyJjbGllbnRJRCI6Imh0dHBzOi8vYXBwbGVoZWFsdGgub3JnLyIsImRldmljZUlEIjoiOTkwYzlhYTBhOWU1OTVjOGNhYTllOTI5OGM2ZmRiYjQ4Njk5OTA4ODU1OWQ0NTk5NWZjNGM4ODQxY2Y1NWY4ZSIsInZlbmRvciI6IkFwcGxlIGdvbGQgc2hpdCIsImR0eXBlIjoiQXBwbGUgZ29sZCBzaGl0IiwibW9kZWwiOiJBcHBsZSBnb2xkIHNoaXQiLCJzZXJpYWwiOiJGQi05OTk5OSIsIm1hY0FkZHJlc3MiOiIwOTowMDo5OTo5OTowOTpGQiIsImlhdCI6MTUyMjYzNTU1NiwiZXhwIjoxNTIyNzIxOTU2LCJhdWQiOiJtaWNyb25ldHMuY29tIiwiaXNzIjoiaHR0cHM6Ly9tc28tcG9ydGFsLm1pY3JvbmV0cy5jb20iLCJzdWIiOiJJbml0aWF0ZSByZWdpc3RyYXRpb24gdG8gb24tYm9hcmQgZGV2aWNlIiwianRpIjoiODRiMDUyOGItZWJiMC00ZWZhLWE5OWItNThjMWJhNjM4NjliIn0.iVd1a8wqax7NUW-q90cARNjjn6I1q2ab7SnjZQGmPLc',
+    'Content-type': 'application/json'
+  }
+}
 const Ajv = require('ajv')
 const ajv = new Ajv()
 const Schema = require('../../schemas/micronets')
 const omitOperationalStateMeta = omit(['logEvents', 'statusCode', 'statusText', '_id', '__v'])
-
+const omitStateMeta = omit(['_id', '__v'])
 export const initialState = {
   micronets: [],
   editTargetIds: {},
@@ -20,7 +28,7 @@ export const initialState = {
 export const getters = {
   editTarget (state) {
     if (!state.editTargetIds) return null
-    const { micronetId, subnetId, deviceId } = state.editTargetIds
+    const {micronetId, subnetId, deviceId} = state.editTargetIds
     const micronet = state.micronets.filter(x => x._id === micronetId)[0]
     if (!subnetId) return micronet
     const subnet = micronet.subnets.filter(x => x.subnetId === subnetId)[0]
@@ -30,78 +38,115 @@ export const getters = {
 
 export const mutations = {
   setMicronets: setState('micronets'),
-  setEditTargetIds (state, { micronetId, subnetId, deviceId }) {
-    state.editTargetIds = { micronetId, subnetId, deviceId }
+  setEditTargetIds (state, {micronetId, subnetId, deviceId}) {
+    state.editTargetIds = {micronetId, subnetId, deviceId}
   },
   replaceMicronet (state, micronet) {
     const index = findIndex(propEq('_id', micronet._id), state.micronets)
     if (index < 0) return state.micronets.push(micronet)
     return state.micronets.splice(index, 1, micronet)
   },
-  setToast (state, { show, value }) {
+  setToast (state, {show, value}) {
     const formattedValue = value.charAt(0).toUpperCase() + value.slice(1)
-    state.toast = { show, value: formattedValue }
+    state.toast = {show, value: formattedValue}
   }
 }
 
 export const actions = {
-  initializeMicronets ({ commit }) {
+  initializeMicronets ({commit}) {
     return axios({
-      ...apiInit,
-      method: 'post',
-      url: `${process.env.BASE_URL}/create-mock-micronet`,
-      data: { subnets: 1, hosts: 3 }
+      ...msoPortalApiInit,
+      method: 'get',
+      url: 'http://localhost:3210/portal/session'
     })
-    .then(({ data }) => {
-      commit('setMicronets', [data])
-    })
+      .then((response) => {
+        let {data} = response.data
+        // data = Object.assign([], omitStateMeta(data))
+        data.map((subscriber, index) => {
+          subscriber = Object.assign({}, omitStateMeta(subscriber))
+          axios({
+            ...apiInit,
+            method: 'post',
+            url: `${process.env.BASE_URL}/create-mock-micronet`,
+            data: {subnets: 1, hosts: 0, subscriber: subscriber}
+          })
+            .then(({data}) => {
+              let mergedMicronet = merge(subscriber, data)
+              commit('setMicronets', [data])
+            })
+        })
+        commit('setMicronets', data)
+        return data
+      })
   },
-  fetchMicronets ({ commit, dispatch }, id) {
+  fetchSubscribers ({commit, dispatch}, id) {
     return axios({
       ...apiInit,
       method: 'get',
       url: id ? `${micronetsUrl}/${id}` : micronetsUrl
     })
-    .then(({ data }) => {
-      if (!id && !data.length) return dispatch('initializeMicronets')
-      commit(id ? 'replaceMicronet' : 'setMicronets', data)
-      return data
-    })
+      .then(({data}) => {
+        if (!id && !data.length) return dispatch('initializeMicronets')
+        commit(id ? 'replaceMicronet' : 'setMicronets', data)
+        return data
+      })
   },
-  upsertMicronet ({ commit }, { id, data }) {
-    let dataFormatCheck = Object.assign(omitOperationalStateMeta(data), { timestampUtc: (new Date()).toISOString() })
+  fetchMicronets ({commit, dispatch}, id) {
+    return axios({
+      ...apiInit,
+      method: 'get',
+      url: id ? `${micronetsUrl}/${id}` : micronetsUrl
+    })
+      .then(({data}) => {
+        if (!id && !data.length) return dispatch('initializeMicronets')
+        commit(id ? 'replaceMicronet' : 'setMicronets', data)
+        return data
+      })
+  },
+  upsertMicronet ({commit}, {id, data}) {
+    let dataFormatCheck = Object.assign(omitOperationalStateMeta(data), {timestampUtc: (new Date()).toISOString()})
     const valid = ajv.validate(Schema.Definitions.Subnet, dataFormatCheck)
-    console.log('\n VALID : ' + JSON.stringify(valid) + '\t\t\t  AJV ERRORS : ' + JSON.stringify(ajv.errors))
     return valid === true ? axios(Object.assign({}, apiInit, {
       method: id ? 'put' : 'post',
       url: id ? `${micronetsUrl}/${id}` : micronetsUrl,
       data
     }))
-    .then(response => commit('replaceMicronet', response.data))
-    .catch(error => {
-      console.log('AXIOS error', error.response)
-    }) : commit('setToast', { show: true, value: ajv.errors[0].message })
+      .then(response => commit('replaceMicronet', response.data))
+      .catch(error => {
+        console.log('AXIOS error', error.response)
+      }) : commit('setToast', {show: true, value: ajv.errors[0].message})
   },
-  saveMicronet ({ state, dispatch }, data) {
-    const { micronetId, subnetId, deviceId } = state.editTargetIds
+  upsertInitMicronet ({commit}, {id, data}) {
+    axios(Object.assign({}, apiInit, {
+      method: 'post',
+      url: id ? `${micronetsUrl}/${id}` : micronetsUrl,
+      data
+    }))
+      .then(response => commit('replaceMicronet', response.data))
+      .catch(error => {
+        console.log('AXIOS error', error.response)
+      })
+  },
+  saveMicronet ({state, dispatch}, data) {
+    const {micronetId, subnetId, deviceId} = state.editTargetIds
     const micronet = find(propEq('_id', micronetId))(state.micronets)
     const subnetIndex = findIndex(propEq('subnetId', subnetId))(micronet.subnets)
     const subnetLens = lensPath(['subnets', subnetIndex])
-    if (!deviceId) return dispatch('upsertMicronet', { id: micronetId, data: set(subnetLens, data, micronet) })
+    if (!deviceId) return dispatch('upsertMicronet', {id: micronetId, data: set(subnetLens, data, micronet)})
     const deviceIndex = findIndex(propEq('deviceId', deviceId), view(subnetLens, micronet).deviceList)
     const deviceLens = lensPath(['subnets', subnetIndex, 'deviceList', deviceIndex])
-    return dispatch('upsertMicronet', { id: micronetId, data: set(deviceLens, data, micronet) })
+    return dispatch('upsertMicronet', {id: micronetId, data: set(deviceLens, data, micronet)})
   },
-  addSubnet ({ state, commit, dispatch }, data) {
-    const { micronetId } = state.editTargetIds
+  addSubnet ({state, commit, dispatch}, data) {
+    const {micronetId} = state.editTargetIds
     return axios({
       ...apiInit,
       method: 'post',
       url: `${process.env.BASE_URL}/add-subnet`,
-      data: { ...data, micronetId }
+      data: {...data, micronetId}
     })
-    .then(() => dispatch('fetchMicronets', micronetId))
-    .then(() => commit('setEditTargetIds', {}))
+      .then(() => dispatch('fetchMicronets', micronetId))
+      .then(() => commit('setEditTargetIds', {}))
   }
 }
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,57 +1,88 @@
 const Micronets = require('../models/micronet')
 const axios = require('axios')
 const omit = require('ramda/src/omit')
-// const findIndex = require('ramda/src/findIndex')
 const R = require('ramda')
-// const { omit } = require('ramda')
-
-const omitOperationalStateMeta = omit(['logEvents', 'statusCode', 'statusText', '_id', '__v'])
+const omitOperationalStateMeta = omit(['logEvents', 'statusCode', 'statusText', '_id', '__v', 'id', 'devices', 'name', 'ssid'])
 
 class Store {
-  constructor(context) {
+  constructor (context) {
     this.publish = context.publish
     this.subscribe = context.subscribe
     this.config = context.config
     this.dispatch = this.dispatch.bind(this)
     this.timestamp = () => (new Date()).toISOString()
   }
+
   dispatch (methodName, data) {
-    console.log('***dispatch***', methodName);
+    console.log('***dispatch***', methodName)
     if (typeof this[methodName] !== 'function') {
       throw new Error(`Dispatched unknown method: ${methodName}`)
     }
-    return this[methodName]({ dispatch: this.dispatch }, data)
+    return this[methodName]({dispatch: this.dispatch}, data)
   }
-  initialize ({ dispatch }) {
+
+  initialize ({dispatch}) {
     return dispatch('connectToMtc')
   }
-  callToMtc ({ dispatch }, message) {
-    const { publish, subscribe } = this
+
+  callToMtc ({dispatch}, message) {
+    const {publish, subscribe} = this
     return new Promise(resolve => {
       function handler (response) {
         if (!response) return void 0
         unsubscribe(handler)
         resolve(response)
       }
+
       const unsubscribe = subscribe(handler)
       if (message) publish(message)
     })
   }
-  connectToMtc ({ dispatch }) {
+
+  connectToMtc ({dispatch}) {
     return dispatch('callToMtc').then(message => {
       if (message.statusCode !== 1)
         throw new Error('Init message not first message received from MTC')
       return message
     })
   }
-  createMockMicronet ({ dispatch }, { body }) {
-    const { odl } = this.config
+
+  createMockMicronet ({dispatch}, {body}) {
+    const {odl} = this.config
     return axios(`${odl.host}:${odl.port}/${odl.initializeUrl}/${body.subnets}/${body.hosts}`)
-      .then(message => dispatch('upsertMicronet', { body: message.data } ))
+      .then(message => body.subscriber ? dispatch('upsertInitMicronet', {
+        body: {
+          message: message.data,
+          subscriber: body.subscriber
+        }
+      }) : dispatch('upsertMicronet', {body: message.data}))
   }
-  upsertMicronet ({ dispatch }, { body, params = {} }) {
-    const message = Object.assign(omitOperationalStateMeta(body), { timestampUtc: this.timestamp() })
+
+  upsertInitMicronet ({dispatch}, {body, params = {}}) {
+    const message = Object.assign(omitOperationalStateMeta(body.message), {timestampUtc: this.timestamp()})
     // console.log(JSON.stringify(message, null, 2))
+    return dispatch('callToMtc', message).then(response => {
+      let mergedMicronet = Object.assign({}, response, body.subscriber)
+      if (response.status >= 1000) {
+        const error = new Error('Failed to create micronet')
+        error.logEvents = response.logEvents
+        error.statusCode = 400
+        throw error
+      }
+      return params.id
+        ? Micronets.findById(params.id).then((data) => {
+          let prevLogEvents = data.logEvents
+          let allLogEvents = R.concat(prevLogEvents, response.logEvents)
+          let updatedResponse = Object.assign(response, {logEvents: allLogEvents})
+          return Micronets.update({_id: params.id}, updatedResponse).then(data => ({data}))
+        })
+        : (new Micronets(mergedMicronet).save().then(data => ({statusCode: 201, data})))
+    })
+  }
+
+  upsertMicronet ({dispatch}, {body, params = {}}) {
+    const message = Object.assign(omitOperationalStateMeta(body), {timestampUtc: this.timestamp()})
+    //message = Object.assign({}, omitMergedMicronetState(message))
     return dispatch('callToMtc', message).then(response => {
       if (response.status >= 1000) {
         const error = new Error('Failed to create micronet')
@@ -63,55 +94,58 @@ class Store {
         ? Micronets.findById(params.id).then((data) => {
           let prevLogEvents = data.logEvents
           let allLogEvents = R.concat(prevLogEvents, response.logEvents)
-          let updatedResponse = Object.assign(response ,{ logEvents: allLogEvents })
-          return Micronets.update({ _id: params.id }, updatedResponse).then(data => ({ data }))
+          let updatedResponse = Object.assign(response, {logEvents: allLogEvents})
+          return Micronets.update({_id: params.id}, updatedResponse).then(data => ({data}))
         })
-        : (new Micronets(response)).save().then(data => ({ statusCode: 201, data }))
+        : (new Micronets(response)).save().then(data => ({statusCode: 201, data}))
     })
   }
+
   queryMicronets () {
-    return Micronets.find().exec().then(data => ({ data }))
+    return Micronets.find().exec().then(data => ({data}))
   }
-  getMicronetById (_, { params }) {
-    return Micronets.findById(params.id).then(data => ({ data }))
+
+  getMicronetById (_, {params}) {
+    return Micronets.findById(params.id).then(data => ({data}))
   }
-  addSubnet ({ dispatch }, { body }) {
-    const { micronetId, subnetId, deviceId, macAddress, subnetName, deviceName, deviceDescription } = body
+
+  addSubnet ({dispatch}, {body}) {
+    const {micronetId, subnetId, deviceId, macAddress, subnetName, deviceName, deviceDescription} = body
     const data = {
       subnetId,
       deviceList: [{
         deviceId,
         timestampUtc: this.timestamp(),
-        mac: { eui48: macAddress }
+        mac: {eui48: macAddress}
       }]
     }
     if (subnetName) data.subnetName = subnetName
     if (deviceName) data.deviceList[0].deviceName = deviceName
     if (deviceDescription) data.deviceList[0].deviceDescription = deviceDescription
     return Micronets.findById(micronetId).then(micronet => {
-      micronet = JSON.parse(JSON.stringify(micronet))
-      const isSubnet = x => x.subnetId === subnetId
-      let subnetIdx = R.findIndex(isSubnet)(micronet.subnets)
-      if (subnetIdx < 0) return R.set(
-        R.lensPath(['subnets', micronet.subnets.length]),
-        data,
-        micronet
-      )
-      const subnet = micronet.subnets[subnetIdx]
-      const isDevice = x => x.deviceId === deviceId
-      const deviceIdx = R.findIndex(isDevice)(subnet.deviceList)
-      data.deviceList = deviceIdx < 0
-        ? R.concat(subnet.deviceList, data.deviceList)
-        : R.adjust(R.merge(R.__, data.deviceList[0]), deviceIdx, subnet.deviceList)
-      return R.set(
-        R.lensPath(['subnets', subnetIdx]),
-        R.merge(subnet, data),
-        micronet
-      )
-    })
-    .then(updated => {
-      return dispatch('upsertMicronet', { body: updated, params: { id: micronetId } })
-    })
+        micronet = JSON.parse(JSON.stringify(micronet))
+        const isSubnet = x => x.subnetId === subnetId
+        let subnetIdx = R.findIndex(isSubnet)(micronet.subnets)
+        if (subnetIdx < 0) return R.set(
+          R.lensPath(['subnets', micronet.subnets.length]),
+          data,
+          micronet
+        )
+        const subnet = micronet.subnets[subnetIdx]
+        const isDevice = x => x.deviceId === deviceId
+        const deviceIdx = R.findIndex(isDevice)(subnet.deviceList)
+        data.deviceList = deviceIdx < 0
+          ? R.concat(subnet.deviceList, data.deviceList)
+          : R.adjust(R.merge(R.__, data.deviceList[0]), deviceIdx, subnet.deviceList)
+        return R.set(
+          R.lensPath(['subnets', subnetIdx]),
+          R.merge(subnet, data),
+          micronet
+        )
+      })
+      .then(updated => {
+        return dispatch('upsertMicronet', {body: updated, params: {id: micronetId}})
+      })
   }
 }
 
