5b84110e34390a80edb1f34f8b0c29a4fd860c72
Ashwini Kadam <a.kadam@cablelabs.com>
2018-05-17 09:33:37 -0700

    AWS url updates

--- client/config/dev.env.js
+++ client/config/dev.env.js
@@ -3,8 +3,8 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_URL: '"http://127.0.0.1:3000"',
-  CLIENT_BASE_URL: '"http://127.0.0.1:8080"',
-  MSO_PORTAL_BASE_URL: '"http://127.0.0.1:3210"',
-  DHCP_BASE_URL: '"http://127.0.0.1:5000"'
+  BASE_URL: '"http://10.142.10.46:3000"',
+  CLIENT_BASE_URL: '"http://10.142.10.46:8080"',
+  MSO_PORTAL_BASE_URL: '"http://10.142.10.46:3210"',
+  DHCP_BASE_URL: '"http://10.36.32.127:5000"'
 })
