b945fc02ba530f62a4674d19d2daba713d95ae1e
Ashwini Kadam <a.kadam@cablelabs.com>
2018-05-25 13:53:48 -0700

    Cleanup

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,12 +2,13 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import storeSpec from './main'
 import createPersistedState from "vuex-persistedstate";
+
 Vue.use(Vuex)
- const store = new Vuex.Store(storeSpec)
+const store = new Vuex.Store(storeSpec)
+
 // const store = new Vuex.Store({
-//   storeSpec: storeSpec,
-//   plugins: [
-//     createPersistedState({ paths: ['deviceLeases']})
-//   ]
+//   ...storeSpec,
+//   plugins: [createPersistedState({ paths: ['deviceLeases']})]
 // });
+
 export default store
