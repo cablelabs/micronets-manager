f9c5faf64d19c91d0b310501c3b6424430697ed5
Ashwini Kadam <a.kadam@cablelabs.com>
2018-08-15 11:22:05 -0700

    Socket IO updates

--- api/index.js
+++ api/index.js
@@ -1,27 +1,46 @@
 /* eslint-disable no-console */
-const logger = require('./logger');
-const app = require('./app');
-const port = app.get('port');
-const server = app.listen(port);
-const io = require('socket.io')(server);
+const logger = require ( './logger' );
+const app = require ( './app' );
+const port = app.get ( 'port' );
+const server = app.listen ( port );
+const io = require ( 'socket.io' ) ( server );
 
-process.on('unhandledRejection', (reason, p) =>
-  logger.error('Unhandled Rejection at: Promise ', p, reason)
+process.on ( 'unhandledRejection' , ( reason , p ) =>
+  logger.error ( 'Unhandled Rejection at: Promise ' , p , reason )
 );
 
-server.on('listening', () =>
-  logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
+server.on ( 'listening' , () =>
+  logger.info ( 'Feathers application started on http://%s:%d' , app.get ( 'host' ) , port )
 );
 
-io.on('connection' , (() => logger.info('SOCKET IO CONNECTION ')))
+io.on ( 'connection' , (() => logger.info ( 'Socket IO connection' )) )
 
-io.on('connection' , (socket) => {
-  app.service('/mm/v1/micronets/users').on('userCreate' ,(data) => {
-     console.log('\n FeatherJS event userCreate fired with data : ' + JSON.stringify(data))
-    socket.emit('userCreate', data);
-  });
-  app.service('/mm/v1/micronets/users').on('userDeviceAdd' ,(data) => {
-     console.log('\n FeatherJS event userDeviceAdd fired with data : ' + JSON.stringify(data))
-    socket.emit('userDeviceAdd', data);
-  });
-});
+app.service ( '/mm/v1/micronets/users' ).on ( 'userCreate' , ( data ) => {
+  console.log ( '\n FeatherJS event userCreate fired with data : ' + JSON.stringify ( data ) )
+  io.on ( 'connection' , ( socket ) => {
+    logger.info ( 'Socket IO connection with data : ' + JSON.stringify ( data ) )
+    socket.emit ( 'userCreate' , data );
+    socket.on ( 'disconnect' , () => {
+      console.log ( '\n Socket IO disconnect' + JSON.stringify ( socket.id ) + 'with Data : ' + JSON.stringify ( data ) )
+      socket.removeAllListeners ( 'send message' );
+      socket.removeAllListeners ( 'disconnect' );
+      socket.removeAllListeners ( 'connection' );
+      socket.disconnect ( true );
+    } );
+  } );
+} );
+app.service ( '/mm/v1/micronets/users' ).on ( 'userDeviceAdd' , ( data ) => {
+  console.log ( '\n FeatherJS event userDeviceAdd fired with data : ' + JSON.stringify ( data ) )
+  io.on ( 'connection' , ( socket ) => {
+    logger.info ( 'Socket IO connection with data : ' + JSON.stringify ( data ) )
+    socket.emit ( 'userDeviceAdd' , data );
+    socket.on ( 'disconnect' , () => {
+      console.log ( '\n Socket IO disconnect' + JSON.stringify ( socket.id ) + 'with Data : ' + JSON.stringify ( data ) )
+      socket.removeAllListeners ( 'send message' );
+      socket.removeAllListeners ( 'disconnect' );
+      socket.removeAllListeners ( 'connection' );
+      socket.disconnect ( true );
+    } );
+  } );
+} );
+
