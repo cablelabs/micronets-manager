064401dce3365a10021fdecfef66110cd58e8a15
Ashwini Kadam <a.kadam@cablelabs.com>
2018-09-15 15:34:54 -0700

    Cleanup

--- api/services/micronets/micronets.service.js
+++ api/services/micronets/micronets.service.js
@@ -26,9 +26,9 @@ module.exports = function ( app ) {
       { req : req } ,
       { params : service.hooks.params }
     )
-    console.log ( '\n MICRO-NETS SERVICE RESULT : ' + JSON.stringify ( result ) )
+    // console.log ( '\n Micronets service result : ' + JSON.stringify ( result ) )
     service.on ( 'created' , ( micronet ) => {
-      console.log ( '\n MICRO-NETS SERVICE INIT CREATE EVENT : ' + JSON.stringify ( micronet ) )
+      console.log ( '\n Micronets service created event : ' + JSON.stringify ( micronet ) )
     } )
     res.json ( result )
   } );
@@ -41,9 +41,9 @@ module.exports = function ( app ) {
         { req : req } ,
         { params : service.hooks.params }
       )
-      console.log ( '\n MICRO-NETS ADD DEVICE TO SUBNET SERVICE RESULT : ' + JSON.stringify ( result ) )
+      // console.log ( '\n Micronets service add device to subnet result : ' + JSON.stringify ( result ) )
       service.on ( 'created' , ( micronet ) => {
-        console.log ( '\n MICRO-NETS ADD DEVICE TO SUBNET SERVICE CREATE EVENT : ' + JSON.stringify ( micronet ) )
+        console.log ( '\n Micronets service add device to subnet created event  : ' + JSON.stringify ( micronet ) )
       } )
       res.json ( result )
     }
@@ -54,25 +54,20 @@ module.exports = function ( app ) {
     const { path , originalUrl , method , params } = req
     console.log ( '\n REQUEST PATH : ' + JSON.stringify ( path ) + '\t\t ORIGINAL URL : ' + JSON.stringify ( originalUrl ) + '\t\t METHOD : ' + JSON.stringify ( method ) + '\t\t PARAMS : ' + JSON.stringify ( params ) )
     if ( method == 'POST' ) {
-      console.log ( '\n METHOD EQUALS : ' + JSON.stringify ( method ) )
-      // const result =  await service.patch ( null,{ req },
-      //   { query : { }, mongoose: { upsert: true }
-      // } )
+
       const result = await service.create (
         { req : req } ,
         { params : service.hooks.params }
       )
-      console.log ( '\n MICRO-NETS ADD SUBNET SERVICE RESULT : ' + JSON.stringify ( result ) )
+      // console.log ( '\n Micronets service add subnet result : ' + JSON.stringify ( result ) )
       service.on ( 'created' , ( micronet ) => {
-        console.log ( '\n MICRO-NETS ADD SUBNET SERVICE CREATE EVENT : ' + JSON.stringify ( micronet ) )
+        console.log ( '\n Micronets service add subnet created event : ' + JSON.stringify ( micronet ) )
       } )
       res.json ( result )
     }
 
     else if ( method == 'GET' ) {
-      console.log ( '\n METHOD EQUALS : ' + JSON.stringify ( method ) )
       const result = await service.get ( { ...params } )
-      console.log ( '\n MICRO-NETS GET SUBNETS SERVICE RESULT : ' + JSON.stringify ( result ) )
       res.json ( result )
     }
 
