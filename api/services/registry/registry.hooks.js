5dfd4108b733aa1e48d0621aeacb24b78c7792c4
Ashwini Kadam <a.kadam@cablelabs.com>
2018-10-14 15:43:19 -0700

    PUT /registry updates

--- api/services/registry/registry.hooks.js
+++ api/services/registry/registry.hooks.js
@@ -1,6 +1,8 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
 const omit = require ( 'ramda/src/omit' );
 const omitMeta = omit ( [ 'updatedAt' , 'createdAt' , '_id' , '__v' ] );
+const apiInit = { crossDomain : true , headers : { 'Content-type' : 'application/json' } }
+const axios = require ( 'axios' );
 module.exports = {
   before: {
     all: [ authenticate('jwt') ],
@@ -8,11 +10,9 @@ module.exports = {
     get: [
       hook => {
       const {params, data, id} = hook
-        console.log('\n Registry get service params.headers : ' + JSON.stringify(params.headers))
-        console.log('\n Registry get service params.id : ' + JSON.stringify(params.id))
-        console.log('\n Registry get service hook.params.query : ' + JSON.stringify(hook.params.query))
+        console.log('\n REGISTRY GET HOOK headers : ' + JSON.stringify(params.headers) + '\t\t params.id : ' + JSON.stringify(params.id) + '\t\t hook.params.query : ' + JSON.stringify(hook.params.query))
         const query = Object.assign({ subscriberId: id ? id : params.id }, hook.params.query);
-        console.log('\n Registry get service query : ' + JSON.stringify(query))
+        console.log('\n REGISTRY GET HOOK Query : ' + JSON.stringify(query))
         hook.params.mongoose = {
           runValidators: true,
           setDefaultsOnInsert: true
@@ -32,7 +32,22 @@ module.exports = {
         hook.data = Object.assign ( {} , data )
       }
     ],
-    update: [],
+    update: [
+      async (hook) => {
+        const {params, data, id} = hook
+        hook.params.mongoose = {
+          runValidators: true,
+          setDefaultsOnInsert: true,
+          upsert : true
+        }
+        console.log('\n UPDATE HOOK REGISTRY PARAMS : ' + JSON.stringify(params) + '\t\t DATA : ' + JSON.stringify(data) + '\t\t\t ID : ' + JSON.stringify(id))
+        let registryToModify = await hook.app.service('/mm/v1/micronets/registry').find(id)
+        registryToModify = registryToModify.data[0]
+        const patchResult = await hook.app.service('/mm/v1/micronets/registry').patch(null, Object.assign({},{...registryToModify, ...data}),{ query : { subscriberId: id }, mongoose: { upsert: true}})
+        hook.result = patchResult[0]
+        return Promise.resolve(hook);
+      }
+    ],
     patch: [],
     remove: []
   },
