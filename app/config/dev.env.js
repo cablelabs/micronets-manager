f586cd33501c6209335f234a829a044a89a2e99c
Ashwini Kadam <a.kadam@cablelabs.com>
2018-10-30 10:28:59 -0600

    Linode config updates

--- app/config/dev.env.js
+++ app/config/dev.env.js
@@ -4,9 +4,9 @@ var prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   BASE_URL: '"http://127.0.0.1:3000"',
-  MM_SERVER_BASE_URL: '"http://10.142.10.46:3030"',
-  CLIENT_BASE_URL: '"http://10.142.10.46:8080"',
-  MSO_PORTAL_BASE_URL: '"http://10.142.10.46:3210"',
+  MM_SERVER_BASE_URL: '"http://198.58.114.200:3030"',
+  CLIENT_BASE_URL: '"http://198.58.114.200:8080"',
+  MSO_PORTAL_BASE_URL: '"http://198.58.114.200:3210"',
   DHCP_BASE_URL: '"http://10.36.32.127:5001"',
   DHCP_SOCKET_URL: '"wss://74.207.229.106:5050/micronets/v1/ws-proxy/grandpa-gw"'
 })
