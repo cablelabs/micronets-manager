bc3b7bbc5fc1d49c5420e170507285c2b0078174
Ashwini Kadam <a.kadam@cablelabs.com>
2018-12-12 16:41:53 -0800

    NCCOE config updates

--- app/config/dev.env.js
+++ app/config/dev.env.js
@@ -5,8 +5,8 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   BASE_URL: '"http://127.0.0.1:3000"',
   MM_SERVER_BASE_URL: '"http://nccoe-mm-api.micronets.in"',
-  CLIENT_BASE_URL: '"https://nccoe-mm.micronets.in"',
-  MSO_PORTAL_BASE_URL: '"https:/nccoe-mso.micronets.in"',
+  CLIENT_BASE_URL: '"http://nccoe-mm.micronets.in"',
+  MSO_PORTAL_BASE_URL: '"http://nccoe-mso-api.micronets.in"',
   DHCP_BASE_URL: '"http://10.36.32.127:5001"',
   DHCP_SOCKET_URL: '"wss://nccoe-mso:5050/micronets/v1/ws-proxy/nccoe-gw"'
 })
