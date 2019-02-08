ce2e360f8f666ca1591ed2ada6c590b3080afb30
Ashwini Kadam <a.kadam@cablelabs.com>
2019-02-07 17:26:17 -0800

    Logger updates

--- api/hooks/log.js
+++ api/hooks/log.js
@@ -12,13 +12,13 @@ module.exports = function () {
   return context => {
     // This debugs the service call and a stringified version of the hook context
     // You can customize the message (and logger) to your needs
-    let message = `Hook Type: ${context.type}  Path: ${context.path}  Method: ${context.method}`;
+    let message = `\n Hook Type: ${context.type}  Path: ${context.path}  Method: ${context.method}`;
     logger.info(message);
     
     if(typeof context.toJSON === 'function' && logger.level === 'debug') {
-      logger.debug('Hook Context : ' + util.inspect(JSON.stringify(context), {colors: false}));
-      logger.debug('Hook.data : ' + JSON.stringify(context.data));
-      logger.debug('Hook.params: ' + JSON.stringify(context.params));
+      logger.debug('\n Hook Context : ' + util.inspect(JSON.stringify(context), {colors: false}));
+      logger.debug('\n Hook.data : ' + JSON.stringify(context.data));
+      logger.debug('\n Hook.params: ' + JSON.stringify(context.params));
     }
     if (context.result) {
       logger.info('Hook.result.data : ' + JSON.stringify(context.result.data));
