8aef9df244a73ec95a19a60a7d32040ae17ef4d3
Ashwini Kadam <a.kadam@cablelabs.com>
2019-02-11 09:53:08 -0800

    Cleanup

--- api/authentication.js
+++ api/authentication.js
@@ -1,7 +1,5 @@
 const authentication = require('@feathersjs/authentication');
 const jwt = require('@feathersjs/authentication-jwt');
-const local = require('@feathersjs/authentication-local');
-
 
 module.exports = function (app) {
   const config = app.get('authentication');
