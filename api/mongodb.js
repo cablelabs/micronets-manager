ce588a580f4c90738f94fa5d25496e5910e46f95
Ashwini Kadam <a.kadam@cablelabs.com>
2019-04-04 10:21:34 -0700

    Logging and docker updates

--- api/mongodb.js
+++ api/mongodb.js
@@ -5,11 +5,11 @@ module.exports = function () {
   const app = this;
   let dbConfig = ''
   if(process.env.NODE_ENV == 'development' && process.env.NETWORK_MODE == 'false') {
-    logger.debug('MongoDB connection : ' + JSON.stringify(app.get('mongodbDocker')) + '\t\t Node environment : ' + JSON.stringify(process.env.NODE_ENV) + '\t\t DOCKER_COMPOSE : ' + JSON.stringify(process.env.NETWORK_MODE));
+    logger.info('MONGODB URI : ' + JSON.stringify(app.get('mongodbDocker')) + '\t\t  ENV : ' + JSON.stringify(process.env.NODE_ENV) + '\t\t NETWORK_MODE : ' + JSON.stringify(process.env.NETWORK_MODE));
     dbConfig = app.get('mongodbDocker');
   }
   else {
-    logger.debug('MongoDB connection : ' + JSON.stringify(app.get('mongodb')) + '\t\t Node environment : ' + JSON.stringify(process.env.NODE_ENV));
+    logger.info('MONGODB URI : ' + JSON.stringify(app.get('mongodb')) + '\t\t  ENV : ' + JSON.stringify(process.env.NODE_ENV));
     dbConfig = app.get('mongodb');
   }
   const promise = MongoClient.connect(dbConfig);
