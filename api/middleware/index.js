8af912de2547fced4c68056eb358056b9e648a20
Ashwini Kadam <a.kadam@cablelabs.com>
2019-05-10 10:59:42 -0700

    Cleanup

--- api/middleware/index.js
+++ api/middleware/index.js
@@ -1,5 +1,15 @@
 // eslint-disable-next-line no-unused-vars
+const logger = require ( './../../api/logger' );
+
 module.exports = function (app) {
   // Add your custom middleware here. Remember that
   // in Express, the order matters.
+  app.use(function(req, res, next) {
+  const { headers, originalUrl } = req
+  logger.debug('\n MM middleware headers : ' + JSON.stringify(headers) + '\t\t originalUrl : ' + JSON.stringify(originalUrl))
+  req.feathers.requestHeaders = headers;
+  req.feathers.requestUrl = originalUrl
+  next();
+});
+
 };
