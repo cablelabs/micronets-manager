fd71e6fa48f129e58c400b23cbb757fc044f517b
Ashwini Kadam <a.kadam@cablelabs.com>
2019-05-28 15:04:58 -0600

    Updates

--- app/config/dev.env.js
+++ app/config/dev.env.js
@@ -5,7 +5,7 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   MM_SERVER_BASE_URL: '"http://45.56.76.21:3030"',
   CLIENT_BASE_URL: '"http://45.56.76.21:8080"',
-  MSO_PORTAL_BASE_URL: '"http://45.33.1.184:3210"',
+  MSO_PORTAL_BASE_URL: '"http://45.79.13.192:3210"',
   SUBSCRIBER_ID: '"7B2A-BE88-08817Z"',
   TEST_ID: `${process.env.VUE_APP_SUBSCRIBER_ID}`
 })
