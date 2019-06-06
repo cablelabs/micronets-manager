e8b93400705abbd5fddd9c5d74fad471e2e0a5ce
Ashwini Kadam <a.kadam@cablelabs.com>
2019-06-06 18:23:50 -0400

    Localhost config

--- app/config/dev.env.js
+++ app/config/dev.env.js
@@ -3,9 +3,9 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  MM_SERVER_BASE_URL: '"http://192.168.0.15:3030"',
-  CLIENT_BASE_URL: '"http://192.168.0.15:8080"',
-  MSO_PORTAL_BASE_URL: '"http://192.168.0.15:3210"',
+  MM_SERVER_BASE_URL: '"http://127.0.0.1:3030"',
+  CLIENT_BASE_URL: '"http://127.0.0.1:8080"',
+  MSO_PORTAL_BASE_URL: '"http://127.0.0.1:3210"',
   SUBSCRIBER_ID: '"7B2A-BE88-08817Z"',
   TEST_ID: `${process.env.VUE_APP_SUBSCRIBER_ID}`
 })
