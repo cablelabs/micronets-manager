b313c53dbfa5fb0c6e314f21e06c8bd97b99d23b
Ashwini Kadam <a.kadam@cablelabs.com>
2019-06-26 11:56:56 -0700

    Linode config updates

--- docker/dev/env.sh
+++ docker/dev/env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export SUBSCRIBER_ID=7B2A-BE88-08817Z
-export IDENTITY_URL=http://10.70.17.163:3230
-export WEB_SOCKET_BASE_URL=wss://10.70.17.163:5050/micronets/v1/ws-proxy/gw
-export MSO_PORTAL_URL=http://mso-api:3210
+export IDENTITY_URL=http://radius.micronets.in:3230
+export WEB_SOCKET_BASE_URL=wss://ws-proxy-api.micronets.in:5050/micronets/v1/ws-proxy/gw
+export MSO_PORTAL_URL=http://45.79.13.192:3210
 export VUE_APP_SUBSCRIBER_ID=7B2A-BE88-08817Z
 export MONGO_CONNECT_URL=mongodb://mm-mongo:27018/micronets
 
